51ab267e8ef49e2c0a9495b4a65c719791232d66
Fangszx <jygrzn@gmail.com>
2021-07-15 21:26:20 +0800

    Initial commit

---
+++ app/src/main/java/com/fangs/shopipipi_pipi/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.fangs.shopipipi_pipi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navBottom = findViewById<BottomNavigationView>(R.id.nav_bottom_bar)
+
+        navBottom.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.nav_item_food ->{
+                    navBottom.setBackgroundResource(R.color.gray)
+                    true
+                }
+                R.id.nav_item_beverage ->{
+                    navBottom.setBackgroundResource(R.color.red)
+                    true
+                }
+                R.id.nav_item_desert ->{
+                    navBottom.setBackgroundResource(R.color.yellow)
+                    true
+                }
+
+                else -> false
+            }
+
+
+        }
+
+    }
+}
