e92dae2fce5d4a19a16eb7de0b76471875544fd0
Fangszx <jygrzn@gmail.com>
2021-07-16 21:40:15 +0800

    add navigation view for drawer

--- app/src/main/java/com/fangs/shopipipi_pipi/MainActivity.kt
+++ app/src/main/java/com/fangs/shopipipi_pipi/MainActivity.kt
@@ -3,11 +3,15 @@ package com.fangs.shopipipi_pipi
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +25,18 @@ class MainActivity : AppCompatActivity() {
 
         val navBottom = findViewById<BottomNavigationView>(R.id.nav_bottom_bar)
         val topToolbar = findViewById<LinearLayout>(R.id.ll_top_toolbar)
+        val ivMenuTopBar = findViewById<ImageView>(R.id.iv_top_bar_menu)
+
+        //drawer
+        val menuDrawer = findViewById<DrawerLayout>(R.id.dl_menu_drawer)
+
+
+        //open drawer on menu image clock
+        ivMenuTopBar.setOnClickListener {
+
+        menuDrawer.openDrawer(GravityCompat.START)
+
+        }
 
 
         navBottom.setOnItemSelectedListener { item ->
