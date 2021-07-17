ce6a3c4cf42167806ddf3520fba24e51f9c99f70
Fangszx <jygrzn@gmail.com>
2021-07-17 19:52:26 +0800

    change status bar according to color of toolbar and bottom nav bar

--- app/src/main/java/com/fangs/shopipipi_pipi/MainActivity.kt
+++ app/src/main/java/com/fangs/shopipipi_pipi/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -41,21 +44,35 @@ class MainActivity : AppCompatActivity() {
         menuDrawer.openDrawer(GravityCompat.START)
         }
 
+        //status bar
+        val window = window
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+
         navBottom.setOnItemSelectedListener { item ->
             when(item.itemId){
                 R.id.nav_item_food ->{
                     navBottom.setBackgroundResource(R.color.gray)
                     topToolbar.setBackgroundResource(R.color.gray)
+                    window.statusBarColor = ContextCompat.getColor(this,R.color.gray);
+
                     true
                 }
                 R.id.nav_item_beverage ->{
                     navBottom.setBackgroundResource(R.color.red)
                     topToolbar.setBackgroundResource(R.color.red)
+                    window.statusBarColor = ContextCompat.getColor(this,R.color.red);
                     true
                 }
                 R.id.nav_item_desert ->{
                     navBottom.setBackgroundResource(R.color.yellow)
                     topToolbar.setBackgroundResource(R.color.yellow)
+                    window.statusBarColor = ContextCompat.getColor(this,R.color.yellow);
                     true
                 }
 
