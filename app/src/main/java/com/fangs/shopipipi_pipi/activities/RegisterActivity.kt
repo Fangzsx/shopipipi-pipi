38ced404c2fed330eccce63ee6b653cf2bfee55b
Fangszx <jygrzn@gmail.com>
2021-07-21 18:13:34 +0800

    apply linear layout for checkbox

--- app/src/main/java/com/fangs/shopipipi_pipi/activities/RegisterActivity.kt
+++ app/src/main/java/com/fangs/shopipipi_pipi/activities/RegisterActivity.kt
@@ -1,12 +1,23 @@
 package com.fangs.shopipipi_pipi.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.fangs.shopipipi_pipi.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        //login
+        val tvLogin = findViewById<TextView>(R.id.tv_login)
+        tvLogin.setOnClickListener {
+            Intent(this, LoginActivity::class.java).also{
+                startActivity(it)
+            }
+
+        }
     }
 }
